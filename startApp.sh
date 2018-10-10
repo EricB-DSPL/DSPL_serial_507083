6e0ef406d747ceef4290fbd20af3b0248f9658d4
Eric Bergstedt <eric.bergstedt@deepsea.com>
2018-10-10 16:28:05 -0700

    Updating to version 2.0.0

--- startApp.sh
+++ startApp.sh
@@ -1,10 +1,16 @@
+sudo modprobe ftdi_sio
+sleep 1
 
-cd /home/pi/Application/DSPL_serial_507083/
+cd /home/pi/Application/DSPL-serial_507083/
+
 
 sudo chmod +x update.sh
 sudo chmod +x ARM_xbox_serial
 sudo chmod +x Controller_GUI
 sudo chmod 777 settings.json
+
+sudo chmod 777 /sys/bus/usb-serial/drivers/ftdi_sio/new_id
+sudo echo "0856 bf02" > /sys/bus/usb-serial/drivers/ftdi_sio/new_id
 
 sudo sh update.sh
 ./ARM_xbox_serial &
